12801a3df3824a78b01a7860776a9c82fe633159
Indri Surya Ningsih <indrisuryaningsih@example.com>
2025-11-08 22:56:24 +0700

    Update webpack publicPath for GitHub Pages

--- starter-project-with-webpack/webpack.common.js
+++ starter-project-with-webpack/webpack.common.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: process.env.NODE_ENV === 'production' ? '/Intermediate_Asah/' : '/',
   },
   module: {
     rules: [
